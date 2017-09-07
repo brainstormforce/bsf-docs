3d8490f51102c722b2eef31a8aa73bd498a27943
Nikhil Chavan <email@nikhilchavan.com>
2017-09-07 16:38:37 +0530

    Correct @package and updated plugin uri

--- documentation-wordpress.php
+++ documentation-wordpress.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * Plugin Name: Documentation WordPress
- * Plugin URI: http://brainstormforce.com/
+ * Plugin URI: https://github.com/brainstormforce/documentation-wordpress
  * Author: Brainstorm Force
  * Author URI: https://www.brainstormforce.com
  * Version: 1.0.0
  * Description: Easily build documentation website with AJAX based live search functionality. This plugin provides shortcodes to display category list & live search input box.
  * Text Domain: doc-wp
  *
- * @package Documentation/Wordpress
+ * @package Documentation_WordPress
  */
 
 // Including class doc loader.
