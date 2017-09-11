e5666e01f94c8f6aa569b15af9eda3d655afb08e
anilj <anilj@bsf.io>
2017-09-11 20:33:32 +0530

    Plugin description changed

--- bsf-docs.php
+++ bsf-docs.php
@@ -4,7 +4,7 @@
  * Plugin URI: http://brainstormforce.com/
  * Author: Brainstorm Force
  * Author URI: https://www.brainstormforce.com
- * Contributors: Anil
+ * Contributors: brainstormforce, Anil
  * Version: 1.0.0
  * Description: Easily build documentation website with AJAX based live search functionality. This plugin provides shortcodes to display category list & live search input box.
  * Text Domain: bsf-docs
