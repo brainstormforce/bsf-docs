6853123ff152b9a7ebce6cf7037619586c0caee1
Pratik <pratikc@brainstormforce.com>
2018-02-28 13:04:10 +0530

    Provided support for Astra sidebar typogrpahy settings

--- classes/class-bsf-docs-widget.php
+++ classes/class-bsf-docs-widget.php
@@ -136,7 +136,7 @@ class Bsf_Docs_Widget extends WP_Widget {
 				'description' => __( 'Widgets in this area will be shown on all docs single posts and cateory.', 'bsf-docs' ),
 				'before_widget' => '<div id="%1$s" class="widget %2$s">',
 				'after_widget' => '</div>',
-				'before_title' => '<h2 class="docs-widget-title">',
+				'before_title' => '<h2 class="widget-title docs-widget-title">',
 				'after_title' => '</h2>',
 			)
 		);
